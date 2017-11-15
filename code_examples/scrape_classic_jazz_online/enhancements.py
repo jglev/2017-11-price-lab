9fa78b4f4bfe674d73e3343dfb44966253022894
Jacob Levernier <jlevern@upenn.edu>
2017-11-15 16:39:46 -0500

    Got enhancement seed working.

--- code_examples/scrape_classic_jazz_online/enhancements.py
+++ code_examples/scrape_classic_jazz_online/enhancements.py
@@ -17,4 +17,16 @@ url_of_page_to_scrape = [
 # Save the file using the filename from the website
 # =============================================================================
 
+# Amend the for loop for MP3 URLs to have song names:
 
+mp3_links_from_website = {}  # Create a blank list. We'll add to this as we go.
+
+for link in links_from_website:
+    link_url = link['href']
+    link_text = link.text
+
+    if link_url.endswith('.mp3'):
+        mp3_links_from_website[link_url] = link_text
+
+for url, title in mp3_links_from_website.items():
+    print(f'Looking at "{title}" (which is from the URL "{url}")...')
